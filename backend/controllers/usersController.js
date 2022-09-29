0524a663557670e494356b5e9804738211b2d52c
sainingo <nickmentor421@gmail.com>
2022-09-29 11:00:06 +0300

    Register validation

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,14 +1,58 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
-const loginUser = (req, res) => {
-    res.send('Login')
- }
+//register
+ const registerUser = asyncHandler ( async (req, res) => {
+   const {name, email, password} = req.body
+   // validate data before submitting
+   if(!name || !email || !password) {
+      res.status(401).json({
+         message:"All fields are required"
+      })
+      // throw new Error("All fields are required1")
+   }
 
- const registerUser = (req, res) => {
-    res.send('Register')
- }
+   // check if user exist b4 register
+   const userExists = await User.findOne({email})
+   if(userExists) {
+      res.status(401).json({
+         message: "User alraedy exist"
+      })
+   }
 
- const getUsers = (req, res) => {
-    res.send('Get users')
+   const user = await User.create({
+      name,
+      email,
+      password
+   })
+
+   //check if user
+   if(user) {
+      res.status(200).json({
+         _id: user.id,
+         name: user.name,
+         email: user.email
+      })
+   }else {
+      res.status(401).json({
+         message: "Invalid user data"
+      })
+      // throw new Error('Invalid user')
+   }
+
+ })
+
+ //login
+ const loginUser = async (req, res) => {
+   res.json({
+     message: "Login"
+   })
+}
+
+ const getUsers = async (req, res) => {
+    res.json({
+      message: "Me"
+    })
  }
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+
+const connectDb = require('./config/db')
 
 const PORT = process.env.PORT || 5000
 
+connectDb()
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 
 app.use('/api/users', require('./routes/userRoutes'))
 
