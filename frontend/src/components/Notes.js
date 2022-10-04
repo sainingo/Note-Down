c484d16e6592158b0e7494a705aeb37647d89a97
sainingo <nickmentor421@gmail.com>
2022-10-04 19:18:17 +0300

    Increase size of notes

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -4,10 +4,10 @@ const Notes = () => {
   return (
     <div>
          <div className='bg-slate-400 w-[60%] mx-auto my-4 p-6 rounded'>
-            <p>This is a platform for taking down your ideas</p>
+            <p className='text-[17px]'>This is a platform for taking down your ideas</p>
             </div>
             <div className='bg-slate-400 w-[60%] mx-auto my-4 p-6 rounded'>
-            <p>This is a platform for taking down your ideas</p>
+            <p className='text-[17px]'>This is a platform for taking down your ideas</p>
             </div>
     </div>
   )
