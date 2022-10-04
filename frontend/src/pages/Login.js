800b376e749e22960517efbddf7ee1b14b45e9ea
sainingo <nickmentor421@gmail.com>
2022-10-04 19:50:53 +0300

    Respoonsive design

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className=' w-[80%] mx-auto my-6'>
       <h3 className='text-2xl text-white text-center underline underline-offset-2'>Please Login</h3>
-      <div className='w-[50%] mx-auto p-4 text-sky-100'>
+      <div className='sm:w-[50%] mx-auto p-4 text-sky-100'>
         <form>
           <div className='grid p-2'>
           <label>Email</label>
@@ -14,7 +14,7 @@ const Login = () => {
           <label>Password</label>
           <input type='password' required placeholder="Enter your email address" className='p-2 rounded-sm mt-1 focus:outline-none focus:border-sky-500 text-black'/>
           </div>
-          <div className='bg-red-400 w-[30%] px-4 py-2 hover:bg-blue-500 ml-2 mt-4 font-bold'>
+          <div className='bg-red-400 sm:w-[30%] px-4 py-2 hover:bg-blue-500 ml-2 mt-4 font-bold'>
             <button>SIGN IN</button>
           </div>
         </form>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -4,7 +4,7 @@ const Register = () => {
   return ( 
     <div className=' w-[80%] mx-auto my-6'>
      <h3 className='text-2xl text-white text-center underline underline-offset-2'>Please Register Below</h3>
-    <div className='w-[50%] mx-auto p-4 text-sky-100'>
+    <div className='sm:w-[50%] mx-auto p-4 text-sky-100'>
       <form>
         <div className='grid p-2'>
         <label>Username</label>
@@ -22,7 +22,7 @@ const Register = () => {
         <label>Confirm Password</label>
         <input type='password' required placeholder="Confirm your password" className='p-2 rounded-sm mt-1 focus:outline-none focus:border-sky-500 text-black'/>
         </div>
-        <div className='bg-blue-500 w-[30%] px-4 py-2 hover:bg-red-400 ml-2 mt-4 font-bold'>
+        <div className='bg-blue-500 sm:w-[30%] px-4 py-2 hover:bg-red-400 ml-2 mt-4 font-bold'>
           <button>SIGN UP</button>
         </div>
       </form>
