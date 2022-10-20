d3f0ad4b2d086d6c3a3974a70b7a1e0edefe8345
sainingo <nickmentor421@gmail.com>
2022-10-20 12:26:15 +0300

    Use axios to create idea

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
 
+const API_URL = 'api/ideas'
+
 //create idea
 const createIdea = async (idea, token) => {
+ const response = await axios.post(API_URL, idea)
+ if(response.data) {
+    localStorage.setItem('idea', JSON.stringify(response.data))
+ }
 
-
+ return response.data
 }
 
 
