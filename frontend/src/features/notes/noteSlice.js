50a867df0f592abcb33c2051adf8070916f5870d
sainingo <nickmentor421@gmail.com>
2022-10-20 12:17:11 +0300

    Add state of the app

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -1,10 +1,34 @@
 import { createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
-export const createNotes = createAsyncThunk('api/ideas', async (idea, {rejectWithValue}) => {
+export const createNotes = createAsyncThunk('api/createIdea', async (idea, {rejectWithValue}) => {
     try {
         
     } catch (error) {
         
     }
 
+})
+
+
+export const getIdea = createAsyncThunk('api/getIdea', async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+const initialState = {
+    ideas: [],
+    isLoading: false,
+    isError: false,
+    message: '',
+    isSuccess: false
+
+}
+
+
+export const noteSlice = createSlice({
+    name: 'ideas',
+    initialState,
 })
