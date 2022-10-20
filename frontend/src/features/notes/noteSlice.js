56b3d243b6ac8a811e82a09338d2ed19a957642c
sainingo <nickmentor421@gmail.com>
2022-10-20 12:12:57 +0300

    Create a notes slice

---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,10 @@
+import { createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+export const createNotes = createAsyncThunk('api/ideas', async (idea, {rejectWithValue}) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+
+})
