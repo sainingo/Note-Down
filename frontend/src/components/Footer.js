32e6e35363ae87ce60327f18d43e25607dbdf332
sainingo <nickmentor421@gmail.com>
2022-11-16 11:33:39 +0300

    Add styling to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-xl text-center text-white w-full h-creen mt-[30%] underline underline-offset-2'>
-        <p>Kantush design 2022</p>
+        <p className='border border-gray-200 rounded-lg p-2'>Kantush design 2022</p>
     </div>
   )
 }
