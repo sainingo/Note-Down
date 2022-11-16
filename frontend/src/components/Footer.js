ad5a0ab8b42986b2913fbb0a694e66ba818377ce
sainingo <nickmentor421@gmail.com>
2022-11-16 11:25:10 +0300

    Add year in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-xl text-center text-white w-full h-creen mt-[30%] underline underline-offset-2'>
-        <p>Kantush design</p>
+        <p>Kantush design 2022</p>
     </div>
   )
 }
